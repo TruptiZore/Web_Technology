999ad00e55a7ab6fe1a2506158641914b730c95c
TruptiZore <zoretrupti08@gmail.com>
2026-02-28 21:35:45 +0530

    JS Experiment_5 uploaded

---
+++ Experiments/Experiment_5/server.js
@@ -0,0 +1,46 @@
+/*const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+    // Set response header
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    // Get current date & time
+    const now = new Date();
+
+    // Send response
+    res.write(`
+        <h1>Hello</h1>
+        <p>Current Date & Time:</p>
+        <h3>${now}</h3>
+    `);
+
+    res.end();
+});
+
+// Start server on port 3000
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
+
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.end("Hello\nCurrent Date & Time: " + new Date());
+}).listen(3000);
+
+console.log("Server running at http://localhost:3000");*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    const now = new Date()           // get current date & time
+    res.send('Trupti Rajgonda Zore. 24UAM316<br>Experiment 3 : Output<br>Hello<br>Current Date & Time : ' + now)  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
