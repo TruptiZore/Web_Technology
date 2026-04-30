b91f3eb6adade8e6d04bb971ec214192be3c073d
TruptiZore <zoretrupti08@gmail.com>
2026-04-30 15:20:14 +0530

    Angular Project EventCalendar

---
+++ Angular Project/EventCalendar/src/app/app.ts
@@ -0,0 +1,12 @@
+// src/app/app.component.ts
+import { Component } from '@angular/core';
+import { CalendarComponent } from './components/calendar/calendar';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [CalendarComponent],
+    template: '<app-calendar></app-calendar>',
+    styleUrls: ['./app.css'], 
+})
+export class AppComponent { }
